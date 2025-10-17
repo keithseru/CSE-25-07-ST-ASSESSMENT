655dff3cdb74a8a0726666e6d5c5f8d66c244cd6
Keith Seruyange <keithseruyange@gmail.com>
2025-10-17 13:52:52 +0300

    Updated the form and sign up

--- keithseruyange/assessment/users/forms.py
+++ keithseruyange/assessment/users/forms.py
@@ -1,5 +1,7 @@
 from django import forms 
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.core.validators import RegexValidator
 from .models import CustomUser
 
 class SignupForm(UserCreationForm):
@@ -18,6 +20,11 @@ class SignupForm(UserCreationForm):
             'placeholder' : 'Email',
             'class': 'form-input',
         })
+    )
+    
+    phone_regex = RegexValidator(
+        regex=r'^(\+?256|0)?[0-9]{9}$',
+        message="Phone number must be in format: '+256712345678' or '0712345678'"
     )
     
     phone_number = forms.CharField(
@@ -46,19 +53,41 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['full_name', 'email', 'phone_number', 'password1', 'password2']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email is already registered!')
+        return email
+    
+    def clean_phone_number(self):
+        phone = self.cleaned_data.get('phone_number')
+        if phone.startswith('0'):
+            normalized_phone = '+256' + phone[1:]
+        elif phone.startswith('+'):
+            normalized_phone = phone
+        else:
+            normalized_phone = '+256' + phone
         
+        if CustomUser.objects.filter(phone_number=normalized_phone).exists():
+            raise forms.ValidationError('This phone number is already registered.')
+        return phone
+    
+    
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.first_name = self.cleaned_data['full_name'].split()[0]
-        if len(self.cleaned_data['full_name'].split()) > 1:
-            user.last_name = ' '.join(self.cleaned_data['full_name'].split()[1:])
+        full_name_parts = self.cleaned_data['full_name'].split()
+        user.first_name = full_name_parts[0]
+        if len(full_name_parts) > 1:
+            user.last_name = ' '.join(full_name_parts[1:])
+        user.email = self.cleaned_data['email']
         user.phone_number = self.cleaned_data['phone_number']
         user.username = self.cleaned_data['email']
         if commit:
             user.save()
         return user
 
-class LoginForm(AuthenticationForm):
+class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.Textarea(attrs={
             'placeholder': 'Email or Phone Number',
@@ -72,3 +101,44 @@ class LoginForm(AuthenticationForm):
             'class':'form-input'
         })
     )
+    
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            # Try to find user by email or phone number
+            user = None
+            
+            # Check if username is email
+            if '@' in username:
+                try:
+                    user = CustomUser.objects.get(email=username)
+                except CustomUser.DoesNotExist:
+                    pass
+            else:
+                # It's a phone number, normalize it
+                if username.startswith('0'):
+                    normalized_phone = '+256' + username[1:]
+                elif username.startswith('+'):
+                    normalized_phone = username
+                else:
+                    normalized_phone = '+256' + username
+                
+                try:
+                    user = CustomUser.objects.get(phone_number=normalized_phone)
+                except CustomUser.DoesNotExist:
+                    pass
+            
+            if user:
+                # Authenticate using the username field
+                self.user_cache = authenticate(username=user.username, password=password)
+                if self.user_cache is None:
+                    raise forms.ValidationError('Invalid credentials')
+            else:
+                raise forms.ValidationError('Invalid credentials')
+        
+        return self.cleaned_data
+
+    def get_user(self):
+        return getattr(self, 'user_cache', None)
